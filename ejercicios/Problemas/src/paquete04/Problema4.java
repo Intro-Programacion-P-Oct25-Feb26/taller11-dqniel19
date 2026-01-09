0806327e069a26e5bd5ae89a88938d2808d504d2
Daniel <danielp4p1@gmail.com>
2026-01-08 20:16:46 -0500

    commit

--- ejercicios/Problemas/src/paquete04/Problema4.java
+++ ejercicios/Problemas/src/paquete04/Problema4.java
@@ -22,16 +22,16 @@ public class Problema4 {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
         int rta;
-        int ci;
-        String nombre;
+        int ci = 0;
+        String nombre = "";
         System.out.println("Ingrese la opción necesitada (1, 2)");
         rta = entrada.nextInt();
 
         if (rta == 1 || rta == 2) {
-            System.out.printf("Ingrese su cédula de identidad");
+            System.out.println("Ingrese su cédula de identidad");
             ci = entrada.nextInt();
             entrada.nextLine();
-            System.out.printf("Ingrese sus nombres completos");
+            System.out.println("Ingrese sus nombres completos");
             nombre = entrada.nextLine();
         }
 
@@ -40,17 +40,42 @@ public class Problema4 {
         } else if (rta == 2) {
             calcularPredio(ci, nombre);
         } else {
-            System.out.println("Opción Invalida");
+            System.out.println("Opción Inválida");
         }
 
     }
 
     public static void calcularValorLuz(int a, String b) {
-        // calcular luz con la formula dada
-        // presentar los datos
-        // hacer otro procedimiento con la otra formula
-        // presentar datos nuevamente
-    }
-    public static void calcularPredio(int a, String b){}
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double valorKv;
+        double numeroKvMes;
+        double total;
+        System.out.println("Ingrese el valor por kilovatio");
+        valorKv = entrada.nextDouble();
+        System.out.println("Ingrese el número de kilovatios consumidos "
+                + "en el mes");
+        numeroKvMes = entrada.nextDouble();
+        total = valorKv * numeroKvMes;
+        System.out.printf("\nREPORTE DE LUZ\n------------------"
+                + "------------\nCliente %s con cédula %d debe cancelar el "
+                + "valor de $%.2f\n", b, a, total);
 
+    }
+    
+    public static void calcularPredio(int a, String b){
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double valorInmueble;
+        double total;
+        System.out.println("Ingrese el valor del inmueble");
+        valorInmueble = entrada.nextDouble();
+        total = valorInmueble * 0.02;
+        System.out.printf("\nREPORTE DE PREDIO\n------------------"
+                + "------------\nCliente %s con cédula %d "
+                + "tiene un inmueble valorado en $%.0f y debe cancelar "
+                + "de predio $%.2f\n", 
+                b, a, valorInmueble, total);
+        
+    }
 }
